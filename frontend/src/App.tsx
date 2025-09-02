fc43c7b02c7ce126b2b19d20666899ce4cb56410
jupiter221208 <jupiter221208@gmail.com>
2025-09-02 10:34:27 -0400

    add initial files and folders

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Chat } from "./components/Chat";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
